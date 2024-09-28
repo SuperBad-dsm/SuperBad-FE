d499f04e9d0063a6d48d43c140dc9b1c76178f62
uiel <parkuiery@gmail.com>
2024-09-28 22:25:06 +0900

    feat :: back icon 추가

--- app/assets/icons/index.ts
+++ app/assets/icons/index.ts
@@ -1,6 +1,7 @@
 export { default as EyeOff } from "./Hide";
 export { default as Eye } from "./Show";
 export { default as LightningIcon } from "./Lightning";
+export { default as BackIcon } from "./Back"
 
 export interface IconProps {
   OnPress?: () => void;
