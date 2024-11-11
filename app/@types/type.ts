1ca799332322f7f4061c76bc63d262ebee0015cd
hyuna <phyunaa0525@gmail.com>
2024-11-11 20:10:36 +0900

    feat :: 상품 api 세팅

--- app/@types/type.ts
+++ app/@types/type.ts
@@ -5,3 +5,27 @@ export interface MenuType {
   name: string;
   to: string;
 }
+
+export enum ProductEnum {
+  옷 = "CLOTHES",
+  굿즈 = "GOODS",
+  가전제품 = "ELECTRONICS",
+  무료나눔 = "FREE",
+}
+
+export interface Product {
+  id: number;
+  title: string;
+  content: string;
+  price: number;
+  category: ProductEnum;
+  createdDate: string;
+  heartCount: number;
+  status: string;
+  imageUrl: string;
+  seller: {
+    userId: string;
+    nickname: string;
+    password: string;
+  };
+}
